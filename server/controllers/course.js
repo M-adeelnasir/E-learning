78951628e4e242ded19943967b5a1972c8fb2588
adeelkbw <adeelnasirkbw@gmail.com>
2022-07-17 03:14:35 +0500

    read single course --server

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -167,3 +167,27 @@ exports.getInstructorCourses = async (req, res) => {
         })
     }
 }
+
+
+exports.getCourse = async (req, res) => {
+    try {
+        const slug = req.params;
+        const course = await Course.findOne({ slug })
+        if (!course) {
+            return res.status(404).json({
+                success: false,
+                msg: "No course found"
+            })
+        }
+        res.json({
+            success: true,
+            data: course
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            success: false,
+            msg: "SERVER ERRPR"
+        })
+    }
+}
--- server/routes/course.js
+++ server/routes/course.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { requireSignIn, checkInstructor, checkAdmin, checkAuth } = require('../middleware/auth')
-const { uploadImage, removeImage, createCourse, getInstructorCourses } = require('../controllers/course')
+const { uploadImage, removeImage, createCourse, getInstructorCourses, getCourse } = require('../controllers/course')
 router.post('/image-upload', requireSignIn, checkInstructor, uploadImage)
 router.post('/remove/course-image', requireSignIn, checkInstructor, removeImage)
 router.post('/course/create', requireSignIn, checkInstructor, createCourse)
 router.get('/instructor/courses', requireSignIn, checkInstructor, getInstructorCourses)
+router.get('/instructor/courses', requireSignIn, checkInstructor, getInstructorCourses)
+router.get('/course/:slug', getCourse)
 
 
 module.exports = router
