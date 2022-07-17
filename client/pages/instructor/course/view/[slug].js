ba9044edf1a9cfc28afbfedf6d0ba217b6900f54
adeelkbw <adeelnasirkbw@gmail.com>
2022-07-17 05:38:38 +0500

    react markdown npm pkg --client

--- client/pages/instructor/course/view/[slug].js
+++ client/pages/instructor/course/view/[slug].js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { Avatar, Tooltip, Button } from 'antd'
 import { EditOutlined, CheckOutlined } from '@ant-design/icons'
+import ReactMarkdown from 'react-markdown'
 import { useRouter } from 'next/router'
 import { getCourse } from '../../../../requests/course'
 import UserNav from '../../../../components/nav/UserNav'
 import TopNav from '../../../../components/TopNav'
 import InstructorRoute from '../../../../components/routes/InstructorRoute'
+
 const Course = () => {
     const [course, setCourse] = useState({})
+    const [ok, setOk] = useState(false)
 
     const router = useRouter()
 
@@ -16,19 +19,24 @@ const Course = () => {
             const { data } = await getCourse(slug)
             console.log(data.data);
             setCourse(data.data)
+            setOk(true)
         } catch (err) {
             console.log(err);
         }
     }
+    useEffect(() => {
+        console.log(router.isReady);
+
+    }, [router.isReady])
+
 
     useEffect(() => {
         if (router.isReady) {
             const slug = router.query.slug
             readCourse(slug)
         }
-        console.log(course);
     }, [router.isReady])
-    const imgUrl = course.image.Location
+
 
     return (
         <InstructorRoute>
@@ -41,11 +49,11 @@ const Course = () => {
 
 
 
-                    <div className="container d-flex flex-column">
+                    {ok && course !== null && <div className="container d-flex flex-column">
                         <div className=" justify-content-between d-flex mb-0">
                             <div className='d-flex m-4'>
                                 <div>
-                                    <Avatar className='' shape='round' size={120} src={imgUrl} />
+                                    <Avatar className='' shape='round' size={120} src={course.image.Location} />
 
                                 </div>
                                 <div className='d-flex flex-column mt-3' style={{ marginLeft: "15px" }}>
@@ -67,15 +75,14 @@ const Course = () => {
                         </div>
 
                         <div className="col-md-10 offset-md-1">
-                            {course.description}
+                            <ReactMarkdown children={course.description} />
                         </div>
-                    </div>
+                    </div>}
 
 
                 </div>
             </div>
 
-            {/* <div>{JSON.stringify(imgUrl, null, 4)}</div> */}
         </InstructorRoute>
     )
 }
