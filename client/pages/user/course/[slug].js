567d0b0a79d3756f93e806ca1dfc661b653d1dcf
adeelkbw <adeelnasirkbw@gmail.com>
2022-07-24 04:29:45 +0500

    UI update on lesson complet --clien

--- client/pages/user/course/[slug].js
+++ client/pages/user/course/[slug].js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router';
 import TopNav from '../../../components/TopNav'
 import axios from 'axios';
-import { Avatar, Menu, Button } from 'antd';
+import { Avatar, Menu, Button, Affix } from 'antd';
 import ReactPlayer from 'react-player'
 import {
     MenuFoldOutlined,
     MenuUnfoldOutlined,
+    CheckCircleFilled,
+    MinusCircleFilled
 } from '@ant-design/icons'
 
 
@@ -17,7 +19,6 @@ const MyCourse = () => {
     const [collapsed, setCollapsed] = useState(false);
     const [clicked, setClicked] = useState()
     const [hideY, setHideY] = useState("hidden")
-    const [showAlert, setShowAlert] = useState(true)
     const [completedLessons, setCompletedLessons] = useState([])
 
 
@@ -56,13 +57,13 @@ const MyCourse = () => {
     }, [hideY, course])
 
 
-    const handleComplete = async () => {
+    const handleMarkComplete = async () => {
         try {
             const { data } = await axios.post('/api/v1/lesson/make-as-complete', {
                 courseId: course._id,
                 lessonId: course.lessons && course.lessons[clicked] && course.lessons[clicked]._id
             })
-            setShowAlert(false)
+            setCompletedLessons([...completedLessons, course.lessons[clicked]._id])
         } catch (err) {
             console.log(err)
         }
@@ -71,60 +72,80 @@ const MyCourse = () => {
 
     const loadMarkedLesson = async () => {
         try {
-            const { data } = await axios.get(`/api/v1/marked-lessons/${course._id}`)
+            const { data } = await axios.get(`/api/v1/marked-lessons/${course && course._id}`)
             console.log(data.data)
             setCompletedLessons(data.data)
+
         } catch (err) {
             console.log(err)
         }
     }
 
-    useEffect(() => {
-        if (course) loadMarkedLesson()
 
-    }, [])
+
+    useEffect(() => {
+        if (course && course._id) loadMarkedLesson()
+    }, [course && course._id])
 
     return (
         <>
+
             <TopNav />
+
+
+
             <div className='container-fluid d-flex flex'>
                 <div style={{
                     maxWidth: 320
 
                 }}>
-                    <Button
-                        onClick={toggleCollapsed}
-                        className="text-primary mb-2 mt-1 btn-block"
-                    >
-                        {collapsed ? <MenuUnfoldOutlined className='btn-block w-100' /> : <MenuFoldOutlined />}{" "}
-                        {!collapsed && "lessons"}
-                    </Button>
+                    <Affix offsetTop={3} onChange={(affixed) => console.log(affixed)}>
+                        <span>
 
-                    <Menu
-                        defaultSelectedKeys={[clicked]}
-                        inlineCollapsed={collapsed}
-                        mode="inline"
-                        style={{ height: '80vh', overflow: 'scroll', overflowX: 'hidden', overflowY: hideY }}
-                    >
-                        {course && course.lessons && course.lessons.map((lesson, index) => (
-                            <Menu.Item
-                                onClick={() => setClicked(index)}
-                                key={index}
-                            ><Avatar>{index + 1}</Avatar> <span style={{ marginLeft: "12px" }}>{lesson.title.substr(0, 30)}</span>
-                            </Menu.Item>
-                        ))}
-                    </Menu>
+                            <Button
+                                onClick={toggleCollapsed}
+                                className="text-primary mb-2 mt-1 btn-block"
+                            >
+                                {collapsed ? <MenuUnfoldOutlined className='btn-block w-100' /> : <MenuFoldOutlined />}{" "}
+                                {!collapsed && "lessons"}
+                            </Button>
+
+                            <Menu
+                                defaultSelectedKeys={[clicked]}
+                                inlineCollapsed={collapsed}
+                                mode="inline"
+                                style={{ height: '80vh', overflow: 'scroll', overflowX: 'hidden', overflowY: hideY }}
+                            >
+                                {course && course.lessons && course.lessons.map((lesson, index) => (
+                                    <Menu.Item
+                                        onClick={() => setClicked(index)}
+                                        key={index}
+                                    >
+                                        <span className='d-flex justify-content-between flex-wrap'>
+                                            <span>
+                                                <Avatar>{index + 1}</Avatar>
+                                                <span style={{ marginLeft: "15px" }}>
+                                                    {lesson.title.substr(0, 30)}
+
+                                                </span>
+                                            </span>
+                                            {completedLessons.includes(lesson._id) ? <CheckCircleFilled className='text-success ' style={{ margin: " auto 0" }} /> : <MinusCircleFilled className='text-danger' style={{ margin: " auto 0" }} />}
+                                        </span>
+                                    </Menu.Item>
+                                ))}
+                            </Menu>
+                        </span>
+                    </Affix>
 
                 </div>
                 <div className="col">{clicked != -1 && clicked !== undefined ? (
 
                     <div>
-                        {showAlert && <div className="alert alert-success d-flex justify-content-between m-0 p-2">
+                        {!completedLessons.includes(course.lessons && course.lessons[clicked] && course.lessons[clicked]._id) && <div className="alert alert-success d-flex justify-content-between m-2 p-2">
                             <p className='mb-0'>{course.lessons && course.lessons[clicked] && course.lessons[clicked].title.substr(0, 30)}</p>
-                            <p className='mb-0' onClick={handleComplete} style={{ textDecoration: 'underline', cursor: 'pointer' }}>Mark As Complete</p>
+                            <p className='mb-0' onClick={handleMarkComplete} style={{ textDecoration: 'underline', cursor: 'pointer' }}>Mark As Complete</p>
                         </div>}
                         <div>
-                            {/* {JSON.stringify(course.lessons && course.lessons[0] && course.lessons[0].video)} */}
                             {course.lessons && course.lessons[clicked] && course.lessons[clicked].video && course.lessons[clicked].video.Location &&
                                 <div className=''>
                                     <ReactPlayer
@@ -132,6 +153,8 @@ const MyCourse = () => {
                                         controls
                                         width="100%"
                                         height="100%"
+                                        playing
+
 
                                     />
                                 </div>
@@ -140,7 +163,10 @@ const MyCourse = () => {
                             {course && course.lessons && course.lessons[clicked] && course.lessons[clicked].content}
                         </div>
                     </div>
-                ) : ("Let Start! Learning Lesson")}</div>
+                ) : (<div className='d-flex align-items-center justify-content-center' style={{ height: '80%' }}>
+                    <h1>Welcome Lets! Start Learning</h1>
+
+                </div>)}</div>
             </div>
         </>
     )
