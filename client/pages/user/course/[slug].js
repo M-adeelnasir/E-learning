954f508fe9fc705cfeced125e2f6e8a51b8f0df1
adeelkbw <adeelnasirkbw@gmail.com>
2022-07-23 20:36:06 +0500

    lessons side bar collaps and uncollaps --client

--- client/pages/user/course/[slug].js
+++ client/pages/user/course/[slug].js
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router';
 import TopNav from '../../../components/TopNav'
 import axios from 'axios';
-import { Avatar, Menu } from 'antd';
+import { Avatar, Menu, Button } from 'antd';
+import ReactPlayer from 'react-player'
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+} from '@ant-design/icons'
 
 
 
@@ -50,17 +55,29 @@ const MyCourse = () => {
             <TopNav />
             <div className='container-fluid d-flex flex'>
                 <div style={{
-                    width: "300px",
-                    marginTop: '10px'
+                    maxWidth: 320
 
                 }}>
+                    <Button
+                        onClick={toggleCollapsed}
+                        className="text-primary mb-2 mt-1 btn-block"
+                    >
+                        {collapsed ? <MenuUnfoldOutlined className='btn-block w-100' /> : <MenuFoldOutlined />}{" "}
+                        {!collapsed && "lessons"}
+                    </Button>
+
                     <Menu
                         defaultSelectedKeys={[clicked]}
                         inlineCollapsed={collapsed}
+                        mode="inline"
                         style={{ height: '80vh', overflow: 'scroll', overflowX: 'hidden', overflowY: hideY }}
                     >
                         {course && course.lessons && course.lessons.map((lesson, index) => (
-                            <Menu.Item onClick={() => setClicked(index)}><Avatar>{index + 1}</Avatar> {lesson.title.substr(0, 30)}</Menu.Item>
+                            <Menu.Item
+                                onClick={() => setClicked(index)}
+                                key={index}
+                            ><Avatar>{index + 1}</Avatar> <span style={{ marginLeft: "12px" }}>{lesson.title.substr(0, 30)}</span>
+                            </Menu.Item>
                         ))}
                     </Menu>
 
